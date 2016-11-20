8b37e220327f35070660ebe787a59d6304b5fedb
lall <detarame@uso.com>
2016-11-21 05:46:37 +0900

    テーブル情報追加

--- a.py
+++ a.py
@@ -20,16 +20,19 @@ page_source = requests.get(url)
 bsObj = BeautifulSoup(page_source.content,"html.parser") #page_source.textだと文字化けしる
 
 #テーブルを指定する
+#ipodatatable[0]は企業名　コード、市場、主幹事、承認日、公開日
 table = bsObj.findAll("table",{"class":"ipodatatable"})[0]
 rows = table.findAll("tr")
 print("-----------------------------")
 print(rows)
 
+#ipodatatable[1]はBB開始日、抽選日,購入申込日
 table = bsObj.findAll("table",{"class":"ipodatatable"})[1]
 rows = table.findAll("tr")
 print("-----------------------------")
 print(rows)
 
+#ipodatatable[5]は幹事
 table = bsObj.findAll("table",{"class":"ipodatatable"})[5]
 rows = table.findAll("tr")
 print("-----------------------------")
